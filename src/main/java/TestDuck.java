675de1854c1906b7c796b3a28c9770cda8da339c
daw1 <daw1>
2022-01-28 17:03:01 +0100

    Initial commit

---
+++ src/main/java/TestDuck.java
@@ -0,0 +1,15 @@
+public class TestDuck {
+
+    public static void main(String[]args){
+
+        int weight = 8;
+        float density = 2.3F;
+        String name = "Donald";
+
+
+
+
+
+    }
+
+}
